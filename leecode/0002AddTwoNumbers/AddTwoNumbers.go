ad3009552b7a5ff2c30da0820852cee5f9d10705
jeffreyhan <jeffreyhan@tencent.com>
2022-03-19 20:11:21 +0800

    add files

---
+++ leecode/0002AddTwoNumbers/AddTwoNumbers.go
@@ -0,0 +1,19 @@
+package leetcode
+
+//```
+//Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//Output: 7 -> 0 -> 8
+//```
+//Explanation: 342 + 465 = 807.
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return nil
+}
