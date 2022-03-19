958995158ee247df562b5c0f800c5b3febe4f8a6
jeffreyhan <jeffreyhan@tencent.com>
2022-03-19 23:55:05 +0800

    add code

--- leecode/0003LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.go
+++ leecode/0003LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.go
@@ -3,6 +3,39 @@ package leetcode
 import "fmt"
 
 func LongestSubstringWithoutRepeatingCharacters(s string) int {
-	fmt.Printf("long s = %+v", s)
-	return 0
+	fmt.Printf("s = %+v\n", s)
+	if len(s) == 0 {
+		return 0
+	}
+	var freq [127]int
+	result, left, right := 0, 0, -1
+	for left < len(s) {
+		fmt.Printf("LongestSubstring  left=%d;	right=%d  ", left, right)
+		print_nozero(freq)
+		if right+1 < len(s) && freq[s[right+1]] == 0 {
+			freq[s[right+1]]++
+			right++
+		} else {
+			freq[s[left]]--
+			left++
+		}
+		result = max(result, right-left+1)
+	}
+	return result
+}
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func print_nozero(nums [127]int) {
+	fmt.Printf(" freq :")
+	for idx, v := range nums {
+		if v > 0 {
+			fmt.Printf("%s => %d	", string(rune(idx)), v)
+		}
+	}
+	fmt.Printf("\n")
 }
